ee50a0e6d8b66630676d567c7a0f3612ece456bc
Allex Wang <allex.wxn@gmail.com>
2021-08-31 09:46:55 +0800

    chore(release): Release v1.1.1

--- Formula/spush.rb
+++ Formula/spush.rb
@@ -1,9 +1,9 @@
 class Spush < Formula
-  desc "A linux/osx remote shell push tool. (based on ssh)"
+  desc     "A linux/osx remote shell push tool. (based on ssh)"
   homepage "https://github.com/allex/spush"
-  url "https://github.com/allex/homebrew-spush/releases/download/1.1.0/spush-darwin-amd64.tgz"
-  sha256 "6eede3a0653abed00da65c98be6580bd51acbbd7e7678f75494c010f94f321e7"
-  version "1.1.0"
+  version  "1.1.1"
+  url      "https://github.com/allex/homebrew-spush/releases/download/1.1.1/spush-amd64.tgz"
+  sha256   "5fedf7139511f2ca8300ca1b862cd8d65b001e1b762b1231166fe937e2f8a4da"
 
   def install
     prefix.install "scp.sh"
