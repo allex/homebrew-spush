12c01c8c91fc21bd00e245fc7702998101715100
Allex Wang <allex.wxn@gmail.com>
2021-09-07 17:09:29 +0800

    chore(release): Release v1.1.1

--- Formula/spush.rb
+++ Formula/spush.rb
@@ -3,7 +3,7 @@ class Spush < Formula
   homepage "https://github.com/allex/spush"
   version  "1.1.1"
   url      "https://github.com/allex/homebrew-spush/releases/download/1.1.1/spush-amd64.tgz"
-  sha256   "128b80b4dcf613a2837825320a8b2c5f7e4de97546ac41bb7f7b881e103ead0e"
+  sha256   "d69811063558d036b5d116b5408d0198cb21691ebc0ce0c9a008aa4f971d345f"
 
   def install
     prefix.install "scp.sh"
